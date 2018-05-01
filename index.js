fff26c1f864787bdf48d9b6a6cc5dcd4a1136c90
shandamckinney <mckinney.shanda@gmail.com>
2018-05-01 18:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
-var a, b
+ar a, b
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
 
 function add(a, b){
-  (a + b)
+  return a + b;
 }
+
+var a, b
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function subtract(a, b){
+  return a - b;
+}
+
+var a, b
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function multiply(a,b){
+  return a * b;
+}
+
+var a, b
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function divide(a, b){
+  return a / b;
+}
+
+var a, b
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function inc(a){
+  return a += 1;
+}
+
+var a, b
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function dec(n){
+  return n -= 1;
+}
+
+var a, b
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
